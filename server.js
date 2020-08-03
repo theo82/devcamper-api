daf2de43d55c79c12d372d416b26f150eb6fa59e
theo82 <theoaristi53@gmail.com>
2020-08-03 08:15:43 +0300

    Using the Express Router

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(400).json({ sucess: true, msg: 'Show all bootcamps' });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ sucess: true, msg: 'Create all bootcamps' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+// Mount router
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 

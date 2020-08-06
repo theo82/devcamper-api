aca86555d59e926f7b6e4ad62753a0f83c7b9387
theo82 <theoaristi53@gmail.com>
2020-08-06 10:52:02 +0300

    Mongoose Error Handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,6 @@
+const errorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
 
 // @desc       Get all bootcamps
 // @route      GET /api/v1/bootcamps
@@ -22,12 +24,17 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({ sucess: false });
+      return new ErrorResponse(
+        `Bootcamp not found with id of ${req.params.id}`,
+        404
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
 };
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
@@ -24,6 +25,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount router
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 

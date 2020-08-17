00e8057b773f24c16b911af86bb6ed120efdbc75
theo82 <theoaristi53@gmail.com>
2020-08-17 18:23:45 +0300

    Advanced Results Middleware

--- controllers/courses.js
+++ controllers/courses.js
@@ -7,28 +7,21 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
-
   if (req.params.bootcampId) {
     // {{URL}}/api/v1/bootcamps/id/courses
     // That will give the courses for each bootcamp
-    query = Course.find({ bootcamp: req.params.bootcampId });
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses,
+    });
   } else {
     // {{URL}}/api/v1/course
     // That will give the bootcamp's name and description in each course request
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description',
-    });
+    res.status(200).json(res.advancedResults);
   }
-
-  const courses = await query;
-
-  res.status(200).json({
-    success: true,
-    count: courses.length,
-    data: courses,
-  });
 });
 
 // @desc    Get course

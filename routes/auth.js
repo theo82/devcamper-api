97841c12128b0a10dc577ea6db6191618f5502b2
theo82 <theoaristi53@gmail.com>
2020-09-01 18:00:58 +0300

    Update User Details

--- routes/auth.js
+++ routes/auth.js
@@ -5,15 +5,20 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controllers/auth');
 
 const router = express.Router();
 
 const { protect } = require('../middleware/auth');
+const { update } = require('../models/User');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 

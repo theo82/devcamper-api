02ca311ff6123dc313a95c8c5d159e7da37bf990
theo82 <theoaristi53@gmail.com>
2020-09-05 07:06:11 +0300

    Get Single Review & Update Seeder

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,6 +13,7 @@ const Bootcamp = require('../models/Bootcamp');
 // Include other resource routers
 
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const router = express.Router();
 const advancedResults = require('../middleware/advancedResults');
@@ -20,6 +21,7 @@ const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
